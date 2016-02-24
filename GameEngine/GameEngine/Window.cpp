f024a0dc1a0dca2000da792ffd73fead73b99cfd
Legendaries <legendwaitforitaries@gmail.com>
2016-02-24 09:11:38 -0700

    Update Window.cpp

--- GameEngine/GameEngine/Window.cpp
+++ GameEngine/GameEngine/Window.cpp
@@ -1,1 +1,9 @@
 #include <Window.h>
+
+Window(string title, int width, int height) {   
+	window = glfwCreateWindow(width, height, title, NULL, NULL);
+}
+
+~Window() {
+	glfwDestroyWindow(window); 
+}
