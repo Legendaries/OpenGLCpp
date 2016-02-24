44a5b311024dcead2e10221e861e05eaae584504
Legendaries <legendwaitforitaries@gmail.com>
2016-02-24 09:12:11 -0700

    Update Window.h

--- GameEngine/GameEngine/Window.h
+++ GameEngine/GameEngine/Window.h
@@ -1,13 +1,14 @@
+#ifndef MATRIX4F_H
+#define	MATRIX4F_H
+
 #include <string>
 #include <GLFW/glfw3.h>
 class Window {
-  public:
-    GLFWwindow* window;
-    inline Window(string title, int width, int height) {
-      window = glfwCreateWindow(width, height, title, NULL, NULL);
-    }
-    inline ~Window() {
-      glfwDestroyWindow(window); 
-    }
-  private:
+	public:
+		GLFWwindow* window;
+    	Window(string title, int width, int height);
+		~Window();
+	private:
 }
+
+#endif
